e66b1f4416b52e33f391b71b3294ff91c00a9764
temospena <temospena@gmail.com>
2024-11-12 18:52:49 +0000

    make available data
    to use here:
    https://www.flowmap.blue/in-browser

--- code/flowmap_test.R
+++ code/flowmap_test.R
@@ -13,7 +13,13 @@ CENTROIDS = CENTROIDS |>
   mutate(lon = Coords[,1],
          lat = Coords[,2]) |> 
   st_drop_geometry() |> 
-  rename(id = group)
+  rename(id = group) |> 
+  mutate(name = id) |> 
+  select(id, name, lat, lon)
+
+  
+CENTROIDS$id[CENTROIDS$id == "Vila Franca de Xira"] = "VFX"
+CENTROIDS$id[CENTROIDS$id == "Setúbal"] = "Setubal"
 rm(Coords)  
 
 TRIPSmode = readRDS("data/TRIPSmode.Rds")
@@ -22,6 +28,9 @@ TRIPS = TRIPSmode |>
          dest = Destination,
          count = Total) |> 
   select(origin, dest, count)
+
+TRIPS$origin[TRIPS$origin == "Vila Franca de Xira"] = "VFX"
+TRIPS$dest[TRIPS$dest == "Setúbal"] = "Setubal"
 
 # plot the flow map
 flowmap <- flowmapblue(
@@ -33,6 +42,7 @@ flowmap <- flowmapblue(
   animation = FALSE
 )
 flowmap
+# Oops… Sorry, but something went wrong.
 
 
 
@@ -42,12 +52,12 @@ flowmap
 
 # using the package data ----------------------------------------------------------------------
 
-locations <- data(ch_locations)
-flows <- data(ch_flows)
-flowmap <- flowmapblue(
+locations <- flowmapblue::ch_locations
+flows <- flowmapblue::ch_flows
+flowmap2 <- flowmapblue(
   locations = locations,
   flows = flows,
-  mapboxAccessToken = Sys.getenv('MAPBOX_PUBLIC_TOKEN'),
+  mapboxAccessToken = Sys.getenv('MAPBOX_API_TOKEN'),
   clustering = TRUE,
   darkMode = TRUE,
   animation = FALSE
